8488785fecd4da84aa4d78f26a44b3a4b28564c8
syakimovich <s0yakimovich@gmail.com>
2022-06-06 14:30:37 +0300

    integration test on adding deck

--- src/test/java/com/learningcards/LearningcardsApplicationTests.java
+++ src/test/java/com/learningcards/LearningcardsApplicationTests.java
@@ -1,13 +1,50 @@
 package com.learningcards;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Sql(statements = "CREATE SEQUENCE IF NOT EXISTS HIBERNATE_SEQUENCE START WITH 1 INCREMENT BY 1;")
 class LearningcardsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final String USERNAME = "user1";
+    private final String PASSWORD = "pass1";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    @WithMockUser(value = USERNAME, password = PASSWORD)
+    void addDeck() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+                        .param("username", USERNAME)
+                        .param("password", PASSWORD)
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/login"));
+        String deckName = "deckname1";
+        mockMvc.perform(MockMvcRequestBuilders.post("/add-deck").param("deckname", deckName).with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/home"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/home").with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attribute("decks",
+                        hasItems(hasProperty("name", equalTo(deckName)))));
+    }
 
 }
